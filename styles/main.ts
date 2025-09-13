afb20a4a5ebb9d685f4be001dac3bdc7f6e1c8e1
giovannitrimmer <giovannitrimmer@fatec.sp.gov.br>
2025-09-13 00:55:53 +0000

    Finalização Aula 12-09-2025

--- styles/main.ts
+++ styles/main.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-export const global = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: { flex: 1, padding: 20, backgroundColor: '#fff' },
   input: { borderWidth: 1, borderColor: '#ccc', padding: 10, marginBottom: 10, borderRadius: 6 },
   button: { padding: 12, backgroundColor: '#0d6efd', borderRadius: 6, alignItems: 'center', marginVertical: 10 },
